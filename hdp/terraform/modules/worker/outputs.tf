47a04a406bc160a0e4a144f049c079e500c0573e
Zachary Smith <Zachary.Smith@oracle.com>
2019-05-16 17:28:21 +0000

    Fixed block volume output for HDFS

--- hdp/terraform/modules/worker/outputs.tf
+++ hdp/terraform/modules/worker/outputs.tf
@@ -1,2 +1,2 @@
-output "block-volume-count" { value = "${ceil(((var.hdfs_usable_in_gbs*3)/var.data_blocksize_in_gbs)/var.worker_node_count)}" }
+output "block-volume-count" { value = "${ceil(((var.hdfs_usable_in_gbs*var.replication_factor)/var.data_blocksize_in_gbs)/var.instances)}" }
 output "block-volume-size" { value = "${var.data_blocksize_in_gbs}" }
