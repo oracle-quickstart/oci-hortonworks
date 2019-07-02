6770a5b4487c3bd7bd56e9a5db5833d1693a1bfa
Zachary Smith <Zachary.Smith@oracle.com>
2019-07-02 21:40:50 +0000

    Removed depracated output

--- hdp/terraform/outputs.tf
+++ hdp/terraform/outputs.tf
@@ -1,2 +1,1 @@
 output "AMBARI URL" { value = "https://${module.utility.public-ip}:9443/" }
-output "DEPLOYMENT COMMAND" { value = "../scripts/hdp_deploy.sh ${module.utility.public-ip} ${var.availability_domain} ${var.worker_instance_shape} ${var.block_volumes_per_worker} ${var.worker_node_count}" }
