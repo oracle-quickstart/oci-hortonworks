9f334ec928a9f59787411eee72fef2030a7720ae
Zachary Smith <Zachary.Smith@oracle.com>
2019-07-03 13:51:58 +0000

    Removed HDF - this is causing a conflict in RM.

--- hdf/terraform/network.tf
+++
@@ -1,8 +0,0 @@
-module "network" { 
-	source			= "modules/network"
-	tenancy_ocid		= "${var.tenancy_ocid}"
-	compartment_ocid 	= "${var.compartment_ocid}"
-	availability_domain	= "${var.availability_domain}"
-	region			= "${var.region}"
-	oci_service_gateway 	= "${var.oci_service_gateway[var.region]}"
-}
